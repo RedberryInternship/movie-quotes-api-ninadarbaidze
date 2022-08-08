f0abc4b2644e424ef0cd86821336492d9c4024d6
ninadarbaidze <ninadarbaidze66@gmail.com>
2022-08-08 12:37:56 +0400

    feat: add login schema

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -5,6 +5,7 @@ import { validateSignupForm } from 'schemas'
 const router = express.Router()
 
 router.post('/signup', validateSignupForm(), signup)
+router.post('/login', validateLoginForm(), login)
 router.post('/auth-google',  authGoogle)
 router.post('/verify-account', verifyAccount)
 router.post('/password-recovery', passwordRecovery)
